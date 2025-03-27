018116e49189cd726e6ee72eb1b9d8fbd242bb58
TvoiConfig <kolliceplay220@gmail.com>
2025-03-27 08:57:27 +0300

    Update Upload form template

--- src/views/UploadTracks.vue
+++ src/views/UploadTracks.vue
@@ -6,14 +6,12 @@ import UploadTrackForm from "@/components/common/tracks/UploadTrackForm.vue";
 
 <template>
   <main class="col-12 p-5 mb-5 min-vh-100">
-    <div class="d-flex pb-3 pt-5 align-items-center border-bottom border-dark border-2">
+    <div class="d-flex pb-3 pt-5 align-items-center">
       <img class="h-photo rounded" :src="PlugImg" alt="PlugImg">
       <h2 class="color-E0 m-2 ">Раздел публикации треков</h2>
     </div>
 
     <UploadTrackForm></UploadTrackForm>
-
-    <div class="d-flex border-bottom border-dark border-2"/>
 
     <form class="d-flex flex-column">
       <h4 class="color-E0 m-2">Загружено вами</h4>
