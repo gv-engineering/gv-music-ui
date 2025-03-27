022e590497b45ccaed26ea0182c4520c83c0379b
TvoiConfig <kolliceplay220@gmail.com>
2025-03-27 09:42:13 +0300

    Create pagination template

--- src/components/common/tracks/ListTracks.vue
+++ src/components/common/tracks/ListTracks.vue
@@ -29,8 +29,8 @@ const selectTrack = (track, tracks) => {
 </script>
 
 <template>
-  <div class="list-tracks">
-    <div v-for="track in tracks" :key="track.id" @click="selectTrack(track, tracks)" class="list-tracks-item d-flex align-items-center px-2 rounded mt-1">
+  <div class="list-tracks border-bottom border-dark border-2">
+    <div v-for="track in tracks" :key="track.id" @click="selectTrack(track, tracks)" class="list-tracks-item d-flex align-items-center px-2 rounded my-1">
       <img class="list-photo rounded shadow" :src="track.cover_url" alt="">
       <div class="track-info">
         <h4 class="color-E0 m-2">{{ track.title }}</h4>
@@ -41,6 +41,43 @@ const selectTrack = (track, tracks) => {
         <LikeBtn></LikeBtn>
       </div>
     </div>
+  </div>
+  <div class="d-flex justify-content-center align-items-center">
+    
+    
+    <div class="d-flex gap-2 align-items-center">
+      <font-awesome-icon
+      :icon="['fas', 'fa-angles-left']"
+      class="pagination-a color-E0"
+      />
+      <font-awesome-icon 
+      :icon="['fas', 'fa-angle-left']"
+      class="pagination-a color-E0"
+      />
+
+      <div class="d-flex gap-3">
+        <a class="pagination-a color-E0">1</a>
+        <a class="pagination-a color-E0">2</a>
+        <a class="pagination-a color-E0">3</a>
+        <a class="pagination-a color-E0">4</a>
+        <a class="pagination-a color-E0">5</a>
+        <a class="pagination-a color-E0">6</a>
+        <a class="pagination-a color-E0">7</a>
+        <a class="pagination-a color-E0">8</a>
+        <a class="pagination-a color-E0">9</a>
+        <a class="pagination-a color-E0">10</a>
+      </div>
+    
+      <font-awesome-icon
+      :icon="['fas', 'fa-angle-right']"
+      class="pagination-a color-E0"
+      />
+      <font-awesome-icon 
+      :icon="['fas', 'fa-angles-right']"
+      class="pagination-a color-E0"
+      />
+    </div>
+
   </div>
 </template>
 
@@ -57,5 +94,14 @@ const selectTrack = (track, tracks) => {
 .list-photo {
   width: 50px;
   height: 50px;
+}
+
+.pagination-a {
+  cursor: pointer;
+  text-decoration: none;
+}
+
+.pagination-a:hover {
+  color: #FF4081;
 }
 </style>
